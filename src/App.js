ea47c3723319c12c22d820ab49e3f07b611816ce
sebin tony <sebin.tony@resolver.com>
2019-06-06 15:48:19 -0400

    Added web3 integration with react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import React, {Component} from 'react';
 //import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './App.css';
+import Web3 from 'web3'
 import {Tabs, Tab} from 'react-bootstrap';
+import { TRAIL_LIST_ABI, TRAIL_LIST_ADDRESS } from './config'
 
 //import views over here
 import CreateAssessment from './components/createAssessment';
 import ViewAssessment from './components/viewAssessment';
 
 
-import ReactDOM from 'react-dom';
-
 class App extends Component {
+
   constructor(props, context) {
     super(props, context);
     this.state = {
       key: 'home',
+      account : '',
+      entryList : [],
+      trailCount : 0
     };
+  }
+
+  componentWillMount() {
+    this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const auditTrailList = new web3.eth.Contract(TRAIL_LIST_ABI, TRAIL_LIST_ADDRESS)
+    const trailCount = await auditTrailList.methods.trailCount().call()
+
+    this.setState({ trailCount })
+    for (var i = 1; i <= trailCount; i++) {
+      const entry = await auditTrailList.methods.trails(i).call();
+      console.log(entry);
+
+      this.setState({
+        entryList: [...this.state.entryList, entry]
+      })
+    }
+    console.log(this.state.entryList);
+    
   }
 
   render(){
